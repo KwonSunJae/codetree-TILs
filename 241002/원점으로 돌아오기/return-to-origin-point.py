a321066b7610d25529eed42428a6a8fdeca280b4
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2024-10-02 19:32:42 +0900

    Title: 원점으로 돌아오기, Time: 418ms, Memory: 26MB, Status: Passed - CodeTree

--- 241002/원점으로 돌아오기/return-to-origin-point.py
+++ 241002/원점으로 돌아오기/return-to-origin-point.py
@@ -8,8 +8,20 @@ for _ in range(N):
     x,y = map(int,input().split())
     coor[(x,y)] = 0
 
-def backtracking(x,y,depth):
-    if depth == N and (x==0 or y == 0):
+def get_dir(x,y,nx,ny):
+    if ny > y :
+        return 0
+    elif ny < y:
+        return 2
+
+    if nx > x:
+        return 1
+    elif nx < x:
+        return 4
+
+
+def backtracking(x,y,depth,dirs):
+    if depth == N and (x==0 or y == 0) and dirs != get_dir(x,y,0,0):
         return 1
 
     sums = 0
@@ -18,11 +30,13 @@ def backtracking(x,y,depth):
             continue
         if not (k[0] == x or k[1] ==y):
             continue
+        if dirs == get_dir(x,y,k[0],k[1]):
+            continue
 
         coor[k] = 1
-        sums += backtracking(k[0],k[1],depth+1)
+        sums += backtracking(k[0],k[1],depth+1,get_dir(x,y,k[0],k[1]) )
         coor[k] = 0
     
     return sums
 
-print(backtracking(0,0,0))
+print(backtracking(0,0,0,-1))
