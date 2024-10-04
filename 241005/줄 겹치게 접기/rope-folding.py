75518e72df808158d279a496487f0780475f0d42
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2024-10-05 00:04:34 +0900

    Title: 줄 겹치게 접기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241005/줄 겹치게 접기/rope-folding.py
@@ -0,0 +1,45 @@
+import bisect
+
+import sys
+
+
+input = sys.stdin.readline
+
+ 
+N,L = map(int, input().split())
+arr = []
+for _ in range(N):
+    arr.append(int(input()))
+
+start,end = 0,L
+cnt = 0
+arr.sort()
+
+ans = 0
+while start != end:
+    mid = (start+end)/2
+    cnt +=1
+    mid_idx = bisect.bisect_left(arr,mid)
+    #print(mid_idx,mid,cnt)
+    if cnt == N:
+        break
+
+    flag = True
+
+    if 2*mid_idx > N:
+        flag = False
+
+    for i in range(1,mid_idx):
+        if not flag:
+            break
+        if mid-arr[i] != arr[mid_idx+(mid_idx-i)]-mid :
+            flag = False
+            break
+
+    if not flag:
+        end = arr[N-cnt]
+    else:
+        ans +=1
+    
+        
+print(ans)
