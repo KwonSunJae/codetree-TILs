d5bb0f901ad9f2c7b018c82a94f5d2966f7ae838
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2024-10-05 18:17:11 +0900

    Title: 번호순으로 사진찍기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241005/번호순으로 사진찍기/take-pictures-in-numerical-order.py
@@ -0,0 +1,38 @@
+import bisect
+
+import sys
+
+
+input = sys.stdin.readline
+
+N,K = map(int,input().split())
+arr = []
+
+
+
+for _ in range(K):
+    a,b = map(int,input().split())
+
+    arr.append((a,b)if a <b else (b,a))
+arr.sort(reverse = True)
+
+start, end = arr.pop()
+ans = 1
+
+while arr:
+    i,hate= arr.pop()
+    #print(start,end, i, hate)
+    if i < end and hate >= end :
+        continue
+    
+    elif i < end and hate < end:
+        end = hate
+    
+    elif i >= end :
+        ans +=1
+        start = end 
+        end = hate
+    
+if end != N:
+    ans +=1
+print(ans)
